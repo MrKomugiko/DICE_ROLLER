d98e0e3ca6bff79070a2e05b39f3ae1ce1a5ff41
Kamil Mąka <Mr.Komugiko@gmail.com>
2021-01-17 02:24:06 +0000

    wstępne zaprojektowanie systemu graficznego preprezentującego karty bogów
    w czasie wyboru karta się powiększa i obraca o 180" pokazując awers z opisem i przyciskami skila, właczonymi lub nie w zaleznosci od posiadanego golda
    ( maksymalnie 1 karta "włączona" jednocześnie

---
+++ Assets/Scripts/God.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName="New God",menuName="Scriptable Object/God")]
+public class God : ScriptableObject
+{
+    public int Index;
+    public Sprite Image;
+    public string Name;
+    public string TotemFullName;
+    public string Description;
+    public string SkillType;
+
+    public int LevelCost;
+    public int Leve2Cost;
+    public int Leve3Cost;
+
+    public string Level1SkillValue;
+    public string Level2SkillValue;
+    public string Level3SkillValue;
+
+}
